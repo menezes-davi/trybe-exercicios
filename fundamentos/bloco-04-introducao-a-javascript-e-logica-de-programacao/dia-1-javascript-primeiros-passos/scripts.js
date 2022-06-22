73e3560842422a4831a1bbc5d231a8b73e19165a
Davi Menezes <rezendemenezes15@gmail.com>
2022-06-22 12:43:42 -0300

    exercicios até o 11

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -74,7 +74,7 @@ const b = 7;
     }
     */
 
-//Exercicio 6
+/*Exercicio 6
 
 const peçaXadrez = "DAMA";
 
@@ -100,4 +100,110 @@ switch (peçaXadrez.toLowerCase()) {
     default:
         console.log("Erro,essa peça não exite!")
         break;
+} */
+
+  /*Exercício 7
+
+  let nota = 75;
+
+  if(nota < 0 || nota > 100){
+      console.log("Erro, nota inválida!")
+  }
+  else if(nota >= 90){
+      console.log("A")
+  }
+  else if(nota >= 80){
+    console.log("B")    
+  }
+  else if(nota >= 70){
+    console.log("C")
+  }
+  else if(nota >= 60){
+    console.log("D")
+  }
+  else if(nota >= 50){
+    console.log("E")
+  }
+  else if(nota < 50){
+    console.log("F")
+  } */
+
+  /*Exercício 8
+
+
+  let a = 2; 
+  let b = 9;
+  let c = 5;
+
+  let par = false
+
+  if(a % 2 == 0 || b % 2 == 0 || c % 2 == 0){
+      par = true;
+  }
+  console.log(par); */
+
+  /*Exercício 9
+
+  let a = 2;
+  let b = 5;
+  let c = 6;
+
+  let impar = false;
+
+  if(a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0 ){
+      impar = true;
+  }
+console.log(impar);  */
+
+/*Exercício 10
+
+const custoProduto = 7;
+const valorDeVenda = 20;
+
+if(custoProduto >=0 && valorDeVenda >=0){
+    const custoTotal = custoProduto * 1.2;
+    const lucro = (valorDeVenda - custoTotal) * 1000;
+    console.log(lucro);
 }
+else{
+    console.log("Erro, os valores não podem ser negativos!");
+};   */
+
+/*Exercício 11
+
+let aliquotaINSS; 
+let aliquotaIR;
+
+const salarioBruto = 7000;
+
+if(salarioBruto <= 1556.94){
+    aliquotaINSS = salarioBruto * 0.08;
+}
+else if(salarioBruto <= 2594.92){
+    aliquotaINSS = salarioBruto * 0.09;
+}
+else if(salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * 0.11;
+}
+else{
+    aliquotaINSS = 570.88;
+}
+
+const salarioBase = salarioBruto - aliquotaINSS;
+
+if(salarioBase <= 1903.98){
+    aliquotaIR = 0;
+}
+else if(salarioBase <= 2826.65){
+    aliquotaIR = (salarioBase * 0.075) - 142.80;
+}
+else if(salarioBase <= 3751.05){
+    aliquotaIR = (salarioBase * 0.15) - 354.80;
+}
+else if(salarioBase <= 4664.68){
+    aliquotaIR = (salarioBase * 0.225) - 636.13;
+}
+else{
+    aliquotaIR = (salarioBase * 0.275) - 869.36;
+};
+console.log("Salário: " + (salarioBase - aliquotaIR));  */
