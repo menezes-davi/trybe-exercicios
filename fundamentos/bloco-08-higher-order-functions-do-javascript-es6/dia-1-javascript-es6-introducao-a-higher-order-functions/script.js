5cbad1f0a44215789cab3a6c2bdb80f807563cdc
Davi Menezes <rezendemenezes15@gmail.com>
2022-07-21 20:17:35 -0300

    exercicios 1 criar email

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,15 @@
+const createEmail = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { fullName, email: `${email}@trybe.com` };
+};
+
+const newEmployees = (name) => {
+    const employees = {
+      id1: name('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: name('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: name('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+  console.log(newEmployees(createEmail));
+  
