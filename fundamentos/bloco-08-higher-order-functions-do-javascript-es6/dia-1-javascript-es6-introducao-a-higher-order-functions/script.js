c5e8766be6c072904b8747713ffc982f0b2a14e7
Davi Menezes <rezendemenezes15@gmail.com>
2022-07-22 00:13:13 -0300

    exercicio 3 nota aluno

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -14,11 +14,33 @@
 
 console.log(newEmployees(createEmail));
    */
-const sortitionResult = (number, func) => {
+/* const sortitionResult = (number, func) => {
     const sortitionNumber = Math.floor((Math.random() * 5) + 1);
     return func(number, sortitionNumber) ? 'Tente novamente' : 'Parabéns você ganhou';
 };
 
 const check = (number, sortitionNumber) => number === sortitionNumber;
 
-console.log(sortitionResult(2, check));
+console.log(sortitionResult(2, check)); */
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswer = (rightAnswer, studentAnswer) => {
+    if (rightAnswer === studentAnswer) {
+        return 1;
+    } if (studentAnswer === 'N.A') {
+        return 0;
+    } 
+    return -0.5;
+};
+
+const returnPoints = (gabarito, respostas, func) => {
+    let count = 0;
+    for (let index = 0; index < gabarito.length; index += 1) {
+        const analisa = func(gabarito[index], respostas[index]);
+        count += analisa;
+    }
+    return `Total de pontos: ${count}`;
+}
+console.log(returnPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswer));
