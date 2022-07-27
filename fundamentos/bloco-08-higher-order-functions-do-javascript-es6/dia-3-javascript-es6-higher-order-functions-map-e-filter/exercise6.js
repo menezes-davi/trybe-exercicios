afb50e71ba29636435140a303bbcaecd60722e94
Davi Menezes <rezendemenezes15@gmail.com>
2022-07-26 21:07:17 -0300

    exercicio 6

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
@@ -62,3 +62,12 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu',
+];
+
+function oldBooks() {
+  return books.filter((book) => (2022 - book.releaseYear > 60).map((book) => book.name));
+};
